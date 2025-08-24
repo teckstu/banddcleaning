9b1c9d7bc3d3d66f2622dcc7c66dac6b11a2de1b
teckstu <altstar@Altstar.localdomain>
2025-08-25 08:09:27 +1000

    update

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.use((req, res, next) => {
     "default-src 'self'; " +
     "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdnjs.cloudflare.com; " +
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com; " +
-    "style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com https://*.googleapis.com; " +
+    "style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com https://fonts.gstatic.com https://*.googleapis.com; " +
     "img-src 'self' data: https: blob:; " +
-    "connect-src 'self' https://*.banddcleaning.com.au https://*.onrender.com; " +
+    "connect-src 'self' https://*.banddcleaning.com.au https://*.onrender.com https://www.bandcleaning-com-au.onrender.com; " +
     "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com data: https://*.gstatic.com https://*.googleapis.com; " +
     "object-src 'none'; " +
     "media-src 'self'; " +
